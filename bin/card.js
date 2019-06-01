cccfaaf4630e4be1b73c6776c2010027be999a41
Ahmed El Gabri <ahmed@gabri.me>
2019-06-01 11:12:44 +0200

    More improvments

--- bin/card.js
+++ bin/card.js
@@ -1,55 +1,8 @@
 #!/usr/bin/env node
 
-// Pull in our modules
-const chalk = require('chalk')
-const boxen = require('boxen')
+'use strict'
 
-// Define options for Boxen
-const options = {
-  padding: 1,
-  margin: 1,
-  borderStyle: 'round',
-}
-
-// Text + chalk definitions
-const data = {
-  name: chalk.white('Ahmed El Gabri /'),
-  handle: chalk.cyan('ahmedelgabri'),
-  work: chalk.white(
-    'Principal Software Developer, Front-End (JavaScript) @LightspeedHQ',
-  ),
-  twitter: chalk.cyan('https://twitter.com/ahmedelgabri'),
-  github: chalk.cyan('https://github.com/ahmedelgabri'),
-  linkedin: chalk.cyan('https://linkedin.com/in/ahmedelgabri'),
-  web: chalk.cyan('https://gabri.me'),
-  npx: chalk.white('npx ahmedelgabri'),
-  labelWork: chalk.white.bold('      Work:'),
-  labelTwitter: chalk.white.bold('   Twitter:'),
-  labelGitHub: chalk.white.bold('    GitHub:'),
-  labelLinkedIn: chalk.white.bold('  LinkedIn:'),
-  labelWeb: chalk.white.bold('       Web:'),
-  labelCard: chalk.white.bold('      Card:'),
-}
-
-// Actual strings we're going to output
-const heading = `${data.name} ${data.handle}`
-const working = `${data.labelWork}  ${data.work}`
-const twittering = `${data.labelTwitter}  ${data.twitter}`
-const githubing = `${data.labelGitHub}  ${data.github}`
-const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
-const webing = `${data.labelWeb}  ${data.web}`
-const carding = `${data.labelCard}  ${data.npx}`
-
-// Put all our output together into a single variable so we can use boxen effectively
-const output = `${heading}
-
-  ${working}
-  ${twittering}
-  ${githubing}
-  ${linkedining}
-  ${webing}
-
-
-  ${carding}`
-
-console.log(chalk.green(boxen(output, options)))
+const fs = require('fs')
+const path = require('path')
+const output = fs.readFileSync(path.join(__dirname, 'output'), 'utf8')
+console.log(output)
---
+++ build.js
@@ -0,0 +1,59 @@
+'use strict'
+
+const fs = require('fs')
+const path = require('path')
+const chalk = require('chalk')
+const boxen = require('boxen')
+
+// Define options for Boxen
+const options = {
+  padding: 1,
+  margin: 1,
+  borderStyle: 'round',
+}
+
+// Text + chalk definitions
+const data = {
+  name: chalk.white('Ahmed El Gabri /'),
+  handle: chalk.cyan('ahmedelgabri'),
+  work: chalk.white(
+    'Principal Software Developer, Front-End (JavaScript) @LightspeedHQ',
+  ),
+  twitter: chalk.cyan('https://twitter.com/ahmedelgabri'),
+  github: chalk.cyan('https://github.com/ahmedelgabri'),
+  linkedin: chalk.cyan('https://linkedin.com/in/ahmedelgabri'),
+  web: chalk.cyan('https://gabri.me'),
+  npx: chalk.white('npx ahmedelgabri'),
+  labelWork: chalk.white.bold('      Work:'),
+  labelTwitter: chalk.white.bold('   Twitter:'),
+  labelGitHub: chalk.white.bold('    GitHub:'),
+  labelLinkedIn: chalk.white.bold('  LinkedIn:'),
+  labelWeb: chalk.white.bold('       Web:'),
+  labelCard: chalk.white.bold('      Card:'),
+}
+
+// Actual strings we're going to output
+const heading = `${data.name} ${data.handle}`
+const working = `${data.labelWork}  ${data.work}`
+const twittering = `${data.labelTwitter}  ${data.twitter}`
+const githubing = `${data.labelGitHub}  ${data.github}`
+const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
+const webing = `${data.labelWeb}  ${data.web}`
+const carding = `${data.labelCard}  ${data.npx}`
+
+// Put all our output together into a single variable so we can use boxen effectively
+const output = `${heading}
+
+  ${working}
+  ${twittering}
+  ${githubing}
+  ${linkedining}
+  ${webing}
+
+
+  ${carding}`
+
+fs.writeFileSync(
+  path.join(__dirname, 'bin/output'),
+  chalk.green(boxen(output, options)),
+)
