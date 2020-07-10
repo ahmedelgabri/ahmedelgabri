ba8817c6fcbdaa190d77cf901a2083a5719d158b
Ahmed El Gabri <ahmed@gabri.me>
2020-07-10 17:28:37 +0200

    Minor tweaks

--- build.js
+++ build.js
@@ -16,28 +16,28 @@ const options = {
 const data = {
   name: chalk.white('Ahmed El Gabri /'),
   handle: chalk.cyan('ahmedelgabri'),
-  work: chalk.white('Tech Lead, Platform @Miro'),
+  work: chalk.white('Tech Lead, Platform @Mirohq'),
   twitter: chalk.cyan('https://twitter.com/ahmedelgabri'),
   github: chalk.cyan('https://github.com/ahmedelgabri'),
   linkedin: chalk.cyan('https://linkedin.com/in/ahmedelgabri'),
   web: chalk.cyan('https://gabri.me'),
   npx: chalk.white('npx ahmedelgabri'),
-  labelWork: chalk.white.bold('      Work:'),
-  labelTwitter: chalk.white.bold('   Twitter:'),
-  labelGitHub: chalk.white.bold('    GitHub:'),
-  labelLinkedIn: chalk.white.bold('  LinkedIn:'),
-  labelWeb: chalk.white.bold('       Web:'),
-  labelCard: chalk.white.bold('      Card:'),
+  labelWork: chalk.white.bold('      Work: '),
+  labelTwitter: chalk.white.bold('   Twitter: '),
+  labelGitHub: chalk.white.bold('    GitHub: '),
+  labelLinkedIn: chalk.white.bold('  LinkedIn: '),
+  labelWeb: chalk.white.bold('       Web: '),
+  labelCard: chalk.white.bold('      Card: '),
 }
 
 // Actual strings we're going to output
 const heading = `${data.name} ${data.handle}`
-const working = `${data.labelWork}  ${data.work}`
-const twittering = `${data.labelTwitter}  ${data.twitter}`
-const githubing = `${data.labelGitHub}  ${data.github}`
-const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
-const webing = `${data.labelWeb}  ${data.web}`
-const carding = `${data.labelCard}  ${data.npx}`
+const working = `${data.labelWork} ${data.work}`
+const twittering = `${data.labelTwitter} ${data.twitter}`
+const githubing = `${data.labelGitHub} ${data.github}`
+const linkedining = `${data.labelLinkedIn} ${data.linkedin}`
+const webing = `${data.labelWeb} ${data.web}`
+const carding = `${data.labelCard} ${data.npx}`
 
 // Put all our output together into a single variable so we can use boxen effectively
 const output = `${heading}
@@ -47,7 +47,6 @@ const output = `${heading}
   ${githubing}
   ${linkedining}
   ${webing}
-
 
   ${carding}`
 
