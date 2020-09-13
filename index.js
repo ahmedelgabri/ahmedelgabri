214b1611d71db1c79e2da1d4ef7091c5bf3015ab
Ahmed El Gabri <ahmed@gabri.me>
2020-09-14 00:29:38 +0200

    Refactor exporting as JS

--- index.js
+++ index.js
@@ -1,15 +1,51 @@
-export const card = `[32m[39m
-[32m   ╭─────────────────────────────────────────────────────────╮[39m
-[32m   │                                                         │[39m
-[32m   │   [37mAhmed El Gabri /[39m[32m [36mahmedelgabri[39m[32m                         │[39m
-[32m   │                                                         │[39m
-[32m   │     [37m[1m      Work: [22m[39m[32m [37mTech Lead, Platform @Mirohq[39m[32m            │[39m
-[32m   │     [37m[1m   Twitter: [22m[39m[32m [36mhttps://twitter.com/ahmedelgabri[39m[32m       │[39m
-[32m   │     [37m[1m    GitHub: [22m[39m[32m [36mhttps://github.com/ahmedelgabri[39m[32m        │[39m
-[32m   │     [37m[1m  LinkedIn: [22m[39m[32m [36mhttps://linkedin.com/in/ahmedelgabri[39m[32m   │[39m
-[32m   │     [37m[1m       Web: [22m[39m[32m [36mhttps://gabri.me[39m[32m                       │[39m
-[32m   │                                                         │[39m
-[32m   │     [37m[1m      Card: [22m[39m[32m [37mnpx ahmedelgabri[39m[32m                       │[39m
-[32m   │                                                         │[39m
-[32m   ╰─────────────────────────────────────────────────────────╯[39m
-[32m[39m`
+const chalk = require('chalk')
+const boxen = require('boxen')
+
+// Define options for Boxen
+const options = {
+  padding: 1,
+  margin: 1,
+  borderStyle: 'round',
+}
+
+// Text + chalk definitions
+const data = {
+  name: chalk.white('Ahmed El Gabri /'),
+  handle: chalk.cyan('ahmedelgabri'),
+  work: chalk.white('Tech Lead, Platform @Mirohq'),
+  twitter: chalk.cyan('https://twitter.com/ahmedelgabri'),
+  github: chalk.cyan('https://github.com/ahmedelgabri'),
+  linkedin: chalk.cyan('https://linkedin.com/in/ahmedelgabri'),
+  web: chalk.cyan('https://gabri.me'),
+  npx: chalk.white('npx ahmedelgabri'),
+  labelWork: chalk.white.bold('      Work: '),
+  labelTwitter: chalk.white.bold('   Twitter: '),
+  labelGitHub: chalk.white.bold('    GitHub: '),
+  labelLinkedIn: chalk.white.bold('  LinkedIn: '),
+  labelWeb: chalk.white.bold('       Web: '),
+  labelCard: chalk.white.bold('      Card: '),
+}
+
+// Actual strings we're going to output
+const heading = `${data.name} ${data.handle}`
+const working = `${data.labelWork} ${data.work}`
+const twittering = `${data.labelTwitter} ${data.twitter}`
+const githubing = `${data.labelGitHub} ${data.github}`
+const linkedining = `${data.labelLinkedIn} ${data.linkedin}`
+const webing = `${data.labelWeb} ${data.web}`
+const carding = `${data.labelCard} ${data.npx}`
+
+// Put all our output together into a single variable so we can use boxen effectively
+const output = `${heading}
+
+  ${working}
+  ${twittering}
+  ${githubing}
+  ${linkedining}
+  ${webing}
+
+  ${carding}`
+
+module.exports = {
+  getCard: () => chalk.green(boxen(output, options)),
+}
