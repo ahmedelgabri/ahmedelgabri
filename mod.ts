2258e442ee85477a29c8a7745677faa5e30e7d18
Ahmed El Gabri <ahmed@gabri.me>
2024-04-01 19:36:35 +0200

    Update formatting

--- mod.ts
+++ mod.ts
@@ -3,31 +3,27 @@ import boxen from "npm:boxen@7.1.1";
 
 const fullname = "Ahmed El Gabri";
 const social = fullname.toLowerCase().replaceAll(" ", "");
-const mastodonHandle = fullname.toLowerCase().split(" ").at(-1);
 const jsrUserName = fullname.toLowerCase().split(" ").at(0);
+const domain = fullname.toLowerCase().split(" ").at(-1);
 
-// Text + pc definitions
 const handle = c.blue(social);
-const work = c.reset("Staff Software Engineer @MiroHQ");
+const work = c.reset(`Staff Software Engineer ${c.yellow("@Mirohq")}`);
 const twitter = c.blue(`https://twitter.com/${handle}`);
 const github = c.blue(`https://github.com/${handle}`);
 const linkedin = c.blue(`https://linkedin.com/in/${handle}`);
-const mastodon = c.blue(`https://mastodon.online/@${mastodonHandle}`);
-const web = c.blue("https://gabri.me");
+const web = c.blue(`https://${domain}.me`);
 const npx = c.reset(`npx ${handle}`);
 const deno = c.reset(
   `deno run --allow-env jsr:@${c.blue(jsrUserName as string)}/card`,
 );
 const labelWork = c.gray("      Work: ");
-const labelTwitter = c.dim(" Twitter: ");
-const labelGitHub = c.dim("  GitHub: ");
-const labelLinkedIn = c.dim("LinkedIn: ");
-const labelWeb = c.dim("     Web: ");
-const labelNode = c.gray("    Node: ");
-const labelDeno = c.gray("    Deno: ");
-const labelMastodon = c.dim("Mastodon: ");
+const labelTwitter = c.dim(" Twitter/X: ");
+const labelGitHub = c.dim("    GitHub: ");
+const labelLinkedIn = c.dim("  LinkedIn: ");
+const labelWeb = c.dim("       Web: ");
+const labelNode = c.gray("      Node: ");
+const labelDeno = c.gray("      Deno: ");
 
-// Actual strings we're going to output
 const working = `${labelWork} ${work}`;
 const twittering = `${labelTwitter} ${twitter}`;
 const githubing = `${labelGitHub} ${github}`;
@@ -35,19 +31,9 @@ const linkedining = `${labelLinkedIn} ${linkedin}`;
 const webing = `${labelWeb} ${web}`;
 const npming = `${labelNode} ${npx}`;
 const denoing = `${labelDeno} ${deno}`;
-const mastodoning = `${labelMastodon} ${mastodon}`;
 
-// Put all our output together into a single variable so we can use boxen effectively
-const output = `${working}
-
-  ${githubing}
-  ${linkedining}
-  ${twittering}
-  ${mastodoning}
-  ${webing}
-
-  ${denoing}
-  ${npming}`;
+const output =
+  `${working}\n\n${githubing}\n${linkedining}\n${twittering}\n${webing}\n\n${denoing}\n${npming}`;
 
 /**
  * Returns a boxed and colored personal card containing personal information
